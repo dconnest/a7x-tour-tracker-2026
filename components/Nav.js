6091e2915cc0aec1ee2b30ab2d1e7920f5e43c76
dconnest <raptor077@comcast.net>
2026-08-01 00:08:27 -0400

    Delete components directory

--- components/Nav.js
+++
@@ -1,5 +0,0 @@
-import Link from 'next/link';
-export default function Nav(){return <header className="siteHeader"><div className="shell nav">
-<Link className="brand" href="/"><span className="brandMark">A7X</span><span>Tour Tracker <b>2026</b></span></Link>
-<nav><Link href="/shows">Shows</Link><Link href="/songs">Songs</Link><Link href="/stats">Statistics</Link><Link href="/heatmap">Heat map</Link><Link href="/compare">Compare</Link></nav>
-</div></header>}
